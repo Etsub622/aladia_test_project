3ac65cf104b7c0ee8628cd2e9888406d3dd002d3
Etsub622 <etsubtaye4@gmail.com>
2024-11-10 12:22:40 +0300

    trying to fix github page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  router: {
+    base: 'aladia_test_project', 
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
